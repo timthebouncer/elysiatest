e5276f55031e923415a7af7b7045859f2ab0e98d
tim.wty <tim.wty@link8tech.tw>
2024-07-08 14:17:17 +0800

    init

--- api/index.tsx
+++ api/index.tsx
@@ -6,7 +6,6 @@ import '../src/env'; // Validate env vars
 
 import { app } from '../src/app';
 
-export const config = { runtime: 'edge' };
 
 export default async function handler(request: Request) {
   return app.fetch(request);
